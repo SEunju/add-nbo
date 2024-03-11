1f7dc8058d11f83b763ca00f335412bd450630ad
SEunju <dmswn5506@hanmail.net>
2024-03-11 12:53:32 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,30 @@ int main(int argc, char **argv) {
 	FILE *fp1, *fp2;
 	uint32_t num1, num2;
 
-	fp1 = fopen(argv[1], "rb");
+
+	if ((fp1 = fopen(argv[1], "rb")) == NULL) {
+	       printf("file open error\n");
+	       return -1;
+	}
 	fread(&num1, sizeof(uint32_t), 1, fp1);
+	if (ftell(fp1) < 4) {
+		printf("file1 size smaller than 4 bytes");
+		return -1;
+	}
 	fclose(fp1);
 
-	fp2 = fopen(argv[2], "rb");
+
+	if ((fp2 = fopen(argv[2], "rb")) == NULL) {
+		printf("file open error\n");
+		return -1;
+	}
 	fread(&num2, sizeof(uint32_t), 1, fp2);
+	if (ftell(fp2) < 4) {
+		printf("file2 size samller than 4 bytes");
+		return -1;
+	}
 	fclose(fp2);
+
 
 	uint32_t a = my_ntohl(num1); //0x03e8
 	uint32_t b = my_ntohl(num2); //0x01f4
