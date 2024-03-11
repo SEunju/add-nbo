8dd1f15d95573bac67f7d6ec1ed18529b22d6f4f
SEunju <dmswn5506@hanmail.net>
2024-03-11 10:33:23 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
-uint32_t my_ntohl(uint32_t a) {
-	uint32_t a1 = (a & 0xFF000000) >> 24;
-	uint32_t a2 = (a & 0x00FF0000) >> 8;
-	uint32_t a3 = (a & 0x0000FF00) << 8;
-	uint32_t a4 = (a & 0x000000FF) << 24;
-	return a4 | a3 | a2 | a1;
-}
+#include "my_ntohl.h"
 
 int main(int argc, char **argv) {
 	FILE *fp1, *fp2;
@@ -22,13 +15,8 @@ int main(int argc, char **argv) {
 	fread(&num2, sizeof(uint32_t), 1, fp2);
 	fclose(fp2);
 
-	//printf("%x \n %x \n", num1, num2);
-       	//num1 = e8030000, num2 = f4010000
-	
 	uint32_t a = my_ntohl(num1); //0x03e8
 	uint32_t b = my_ntohl(num2); //0x01f4
 	uint32_t c = a + b;
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", a, a, b, b, c, c); 
-		
-
 }
